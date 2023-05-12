4968ca517227df4ca98034cae450859fa0b47841
myepark <myepark@gmarket.com>
2023-05-13 00:09:46 +0900

    fix build error

--- userservice/src/main/java/com/example/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,13 +2,14 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient // Eureka Client
-public class UserserviceApplication {
+@EnableDiscoveryClient
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
